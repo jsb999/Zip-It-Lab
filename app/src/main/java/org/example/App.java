4ccf4b5435d30192fcaadf6062f04c91de7e5feb
jsb999 <90780236+jsb999@users.noreply.github.com>
2025-12-02 05:39:58 +0000

    done?

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,44 @@
  */
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.List;
+import java.util.ArrayList;
 
+public class App {
+    public static <T> List<T> zip(List<T> list1, List<T> list2){
+        List<T> result = new ArrayList<>();
+
+        int size = 0;
+        if (list1.size() != 0 && list2.size() != 0){
+            if (list1.size() > list2.size()){
+            size = list2.size();
+        } else{
+            size = list1.size();
+            }
+        }
+        
+
+        for (int i = 0; i < size; i++) {
+            if (i < list1.size()) {
+                result.add(list1.get(i));
+            }
+            if (i < list2.size()) {
+                result.add(list2.get(i));
+            }
+        }
+        
+        return result;
+    }
+    
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        List<Integer> nums1 = List.of(1, 3, 5, 7);
+          List<Integer> nums2 = List.of(2, 4, 6, 8);
+          List<Integer> mergedNumbers = zip(nums1, nums2);
+          System.out.println(mergedNumbers); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+          List<String> colors1 = List.of("Red", "Green", "Blue");
+          List<String> colors2 = List.of("White", "Black", "Orange", "Pink", "Fuschia");
+          List<String> mergedWords = zip(colors1, colors2);
+          System.out.println(mergedWords); // ["Red", "White", "Green", "Black", "Blue", "Orange", "Pink", "Fuschia"]
     }
 }
